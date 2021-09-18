070c3a29a9f9a5c39d37bf161021fa8e6b108f79
Brian Kirkpatrick <code@tythos.net>
2021-09-18 21:11:22 +0000

    adding key mount (ro) for push

--- fed.bat
+++ fed.bat
@@ -1,4 +1,4 @@
 @echo off
 rem Uses Docker to launch a bash prompt from a Fedora image, in which %USERPROFILE% is mounted as /home
 docker build -t fed "%~dp0\."
-docker run -it -v "%USERPROFILE%":/home:rw fed
+docker run -it -v "%USERPROFILE%":/home:rw -v "%USERPROFILE%\.ssh":/root/.ssh:ro fed
